ff88ff9d7553c2132ce399fa7a0ef850f5e09ad4
Todd Chen <toddchen@nd.com.cn>
2020-07-26 11:46:29 +0800

    feat: add redux devtools & hot module

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { devToolsEnhancer } from 'redux-devtools-extension'
 import tasks from './reducers'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(tasks)
+const store = createStore(tasks, devToolsEnhancer())
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,6 +18,27 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
+
+// hot module
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default
+
+    ReactDOM.render(
+      <Provider store={store}>
+        <React.StrictMode>
+          <NextApp />
+        </React.StrictMode>
+      </Provider>,
+      document.getElementById('root')
+    );
+  })
+
+  module.hot.accept('./reducers', () => {
+    const nextRootReducer = require('./reducers').default
+    store.replaceReducer(nextRootReducer)
+  })
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
